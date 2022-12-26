2a46bbb8472f26955dfac577851c26a3af7db4bb
Nadish Maredia <nadishns97@gmail.com>
2022-12-26 12:04:33 -0500

    Search and Filter

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@
 const Product = require('../models/productModel');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
+const ApiFeatures = require('../utils/apiFeatures');
 
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
@@ -14,7 +15,9 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 });
 
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-    const products = await Product.find();
+
+    const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter();
+    const products = await apiFeature.query;
     res.status(200).json({
         success: true,
         products
